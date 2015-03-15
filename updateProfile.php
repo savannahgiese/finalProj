8808f1cb623c44ca5bdf3b8b897fb73785bca36b
Savannah Giese <gieses@onid.oregonstate.edu>
2015-03-14 22:26:30 -0700

    PHP page that updates profile picture and nickname

--- updateProfile.php
+++ updateProfile.php
@@ -3,22 +3,36 @@ session_start();
 include 'database.php';
 
 $username = $_SESSION['username'];
-$pic = mysqli_real_escape_string($mysqli, trim($_POST["pic"]));
-$results = array('status' => NULL, 'pic' => NULL);
+$results = array('status' => NULL, 'pic' => NULL, 'name' => NULL);
 
-if (!($stmt = $mysqli->prepare("UPDATE `users` SET `picture` = (?) WHERE `username` = ?"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+if(isset($_POST['pic'])){
+    $pic = mysqli_real_escape_string($mysqli, trim($_POST['pic']));
+    if (!($stmt = $mysqli->prepare("UPDATE `users` SET `picture` = (?) WHERE `username` = ?"))) {
+        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    if (!($stmt->bind_param("ss", $pic, $username))){
+        echo "Binding parameters failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    if (!$stmt->execute()) {
+        echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    //echo "<div id=\"profilePic\"><p><img alt=\"Picture\" id=\"picture\" src=\"" . $userPic . "\"></p>";
+    $results['pic'] = $pic;
 }
-if (!($stmt->bind_param("ss", $pic, $username))){
-    echo "Binding parameters failed: (" . $mysqli->errno . ") " . $mysqli->error;
+if (isset($_POST['name'])){
+    $name = mysqli_real_escape_string($mysqli, trim($_POST['name']));
+    if (!($stmt = $mysqli->prepare("UPDATE `users` SET `nickname` = (?) WHERE `username` = ?"))) {
+        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    if (!($stmt->bind_param("ss", $name, $username))){
+        echo "Binding parameters failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    if (!$stmt->execute()) {
+        echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    //echo "<div id=\"profilePic\"><p><img alt=\"Picture\" id=\"picture\" src=\"" . $userPic . "\"></p>";
+    $results['name'] = $name;
 }
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-//echo "<div id=\"profilePic\"><p><img alt=\"Picture\" id=\"picture\" src=\"" . $userPic . "\"></p>";
-    
-$results['pic'] = $pic;
-$results['status'] = 'right';
 $mysqli->close();
 echo json_encode($results);
 ?>
