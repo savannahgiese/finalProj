a231ca56658bb87f1993b705e6439736700f3430
Savannah Giese <gieses@onid.oregonstate.edu>
2015-03-14 22:25:21 -0700

    Updated welcome page to check if username is set

--- welcome.php
+++ welcome.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 'On');
 include 'password.php';
+include 'database.php';
 session_start();
 ?>
 
@@ -8,9 +9,9 @@ session_start();
 <html>
 
 <head>
-  <title>Sherlock Trivia</title>
-  <link rel="stylesheet" href="website.css">
-  <meta charset="UTF-8">
+<title>Sherlock Trivia</title>
+<link rel="stylesheet" href="website.css">
+<meta charset="UTF-8">
 </head>
 <body>
 <div id="wrap">
@@ -23,16 +24,31 @@ session_start();
 <?php 
     if(session_status() == PHP_SESSION_ACTIVE){
     if(isset($_SESSION['username']) && $_SESSION['username'] != NULL){
-        echo "<ul>
-	       <li><a href=\"http://savvyg.me/Final_Project/welcome.php\">Home</a></li>
-	       <li><a href=\"http://savvyg.me/Final_Project/user.php\">User</a></li>
-	       <li><a href=\"http://savvyg.me/Final_Project/play.php\">Play</a></li>
-	       <li><a href=\"http://savvyg.me/Final_Project/logout.php\">Logout</a></li></ul></div>
-	       <div id=\"content\"><p><img alt=\"Picture\" id=\"picture\" src=\"welcome.gif\"></p>
-        <h2>Welcome, " . $_SESSION['username'] . "</h2></div>";
-    }else{
-        header("Location: http://savvyg.me/Final_Project/mainpage.php");
+    $username = $_SESSION['username'];
+    if (!($stmt = $mysqli->prepare("SELECT `nickname` FROM `users` WHERE username = ?"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
+    if (!($stmt->bind_param("s", $username))){
+        echo "Binding parameters failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    if (!$stmt->execute()) {
+        echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    
+    $stmt->bind_result($name);
+    while ($stmt->fetch()) {
+        $nickname = $name;
+        }
+            echo "<ul>
+	           <li><a href=\"http://savvyg.me/Final_Project/welcome.php\">Home</a></li>
+	           <li><a href=\"http://savvyg.me/Final_Project/user.php\">User</a></li>
+	           <li><a href=\"http://savvyg.me/Final_Project/play.php\">Play</a></li>
+	           <li><a href=\"http://savvyg.me/Final_Project/logout.php\">Logout</a></li></ul></div>
+	           <div id=\"content\"><p><img alt=\"Picture\" id=\"picture\" src=\"welcome.gif\"></p>
+            <h2>Welcome, " . $nickname . "</h2></div>";
+        }else{
+            header("Location: http://savvyg.me/Final_Project/mainpage.php");
+        }
     }
 ?>
 </div>
